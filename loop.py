fc36e48610c937c084bf9e5aed0cd46357776c31
kavikon08-cyber <kavikon08@gmail.com>
2026-02-18 11:22:17 +0530

    Add files via upload

---
+++ loop.py
@@ -0,0 +1,23 @@
+languages=['swift','phython','go',]
+for io in languages:
+    print(io)
+    print('____')
+    
+language="phython"
+for a in language:
+    print(a)
+for i in range(0,4):
+    print('hi')
+    
+languages=["swift","phython","Go","c++"]
+
+for lang in languages:
+    if lang == "Go":
+        continue
+    print(lang)
+
+attributes=["electric","fast"]
+cars=["tesla","porsche","mercedas"]
+for attribute in attributes:
+    for car in cars:
+        print(attribute,car)
