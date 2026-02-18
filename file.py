8510a04497c41160f832bedfa8c41c43e17b2cf4
kavikon08-cyber <kavikon08@gmail.com>
2026-02-18 11:04:50 +0530

    Add files via upload

---
+++ file.py
@@ -0,0 +1,23 @@
+a=open("C://Users//ELCOT//Desktop//file//kavi.txt","w")
+a=open("C://Users//ELCOT//Desktop//file//kavi1.txt","w")
+a.write("This is kavi file")
+a=write("Today is kavi")
+a.close()
+print(a.read(15))
+import os
+os.remove("C://Users//ELCOT//Desktop//file//kavi1.txt")
+print("ok")
+
+"""a=open("C://Users//ELCOT//Desktop//file//kavi.txt","a")
+a.write("today is very bad")
+
+a.close()
+print("ok")
+
+a=open("C://Users//ELCOT//Desktop//file//kavi.txt")
+print(a.read(10))
+
+import os
+os.remove("C://Users//ELCOT//Desktop//file//kavi1.txt")
+print("ok")"""
+
