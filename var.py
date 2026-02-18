16ccf3b661014df85b09d08827b76a5a977516b0
kavikon08-cyber <kavikon08@gmail.com>
2026-02-18 11:29:50 +0530

    Add files via upload

---
+++ var.py
@@ -0,0 +1,16 @@
+x = 5
+y = 7
+z =-8
+print(x+y+z)
+
+
+num=25
+print(num)
+
+username="admin"
+password="12345"
+if username==":"and password ==":":
+    print("Login are successfull")
+else:
+    print("login are dismatched")
+ 
